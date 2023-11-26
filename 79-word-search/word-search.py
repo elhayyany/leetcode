95eb76b0288ef51050e3b3a127306e4e0385b654
Abdelaziz Elhayyany <100360875+elhayyany@users.noreply.github.com>
2023-11-26 11:41:13 +0100

    Time: 6505 ms (31.15%) | Memory: 16.4 MB (44.39%) - LeetSync

--- 79-word-search/word-search.py
+++ 79-word-search/word-search.py
@@ -2,12 +2,17 @@ class Solution:
     def search(self, board, word, i, j, ch, visited):
         if ch == len(word):
             return True
-        if (i, j) in visited or i == -1 or j == -1 or i == len(board) or j == len(board[0]) \
-            or board[i][j] != word[ch]:
+        if  (i, j) in visited or i == -1 or\
+            j == -1 or i == len(board) or\
+            j == len(board[0]) or\
+            board[i][j] != word[ch]:
             ch -= 1
             return False
         visited.add((i, j))
-        if self.search(board, word, i, j + 1, ch+1, visited) or self.search(board, word, i, j - 1, ch+1, visited) or self.search(board, word, i - 1, j, ch+1, visited) or self.search(board, word, i + 1, j, ch+1, visited):
+        if  self.search(board, word, i, j + 1, ch+1, visited) or\
+            self.search(board, word, i, j - 1, ch+1, visited) or\
+            self.search(board, word, i - 1, j, ch+1, visited) or\
+            self.search(board, word, i + 1, j, ch+1, visited):
             return True
         visited.remove((i,j))
         return False
