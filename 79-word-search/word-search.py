1f4033592f726840b76902a8a72c23867070a4b5
Abdelaziz Elhayyany <100360875+elhayyany@users.noreply.github.com>
2023-11-26 11:49:38 +0100

    Time: 5782 ms (52.66%) | Memory: 16.4 MB (44.39%) - LeetSync

--- 79-word-search/word-search.py
+++ 79-word-search/word-search.py
@@ -1,26 +1,25 @@
 class Solution:
-    def search(self, board, word, i, j, ch, visited):
+    def search(self, board, word, i, j, ch):
         if ch == len(word):
             return True
-        if  (i, j) in visited or i == -1 or\
-            j == -1 or i == len(board) or\
-            j == len(board[0]) or\
+        if  j == -1 or i == len(board) or\
+            j == len(board[0]) or  i == -1 or\
             board[i][j] != word[ch]:
-            ch -= 1
             return False
-        visited.add((i, j))
-        if  self.search(board, word, i, j + 1, ch+1, visited) or\
-            self.search(board, word, i, j - 1, ch+1, visited) or\
-            self.search(board, word, i - 1, j, ch+1, visited) or\
-            self.search(board, word, i + 1, j, ch+1, visited):
+        k = board[i][j]
+        board[i][j] = '.'
+        if  self.search(board, word, i, j + 1, ch+1) or\
+            self.search(board, word, i, j - 1, ch+1) or\
+            self.search(board, word, i - 1, j, ch+1) or\
+            self.search(board, word, i + 1, j, ch+1):
             return True
-        visited.remove((i,j))
+        board[i][j] = k
         return False
             
     def exist(self, board: List[List[str]], word: str) -> bool:
         for i in range(len(board)):
             for j in range(len(board[0])):
                 if board[i][j] == word[0]:
-                    if self.search(board, word, i, j, 0, set()):
+                    if self.search(board, word, i, j, 0):
                         return True
         return False
