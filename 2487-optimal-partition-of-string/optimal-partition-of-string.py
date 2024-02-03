60f1451c6b59c20cb4eaa56b94d14c46da1c9ed7
Abdelaziz Elhayyany <100360875+elhayyany@users.noreply.github.com>
2024-02-03 11:04:29 +0100

    Time: 97 ms (57.61%) | Memory: 17.3 MB (75.44%) - LeetSync

--- 2487-optimal-partition-of-string/optimal-partition-of-string.py
+++ 2487-optimal-partition-of-string/optimal-partition-of-string.py
@@ -2,12 +2,10 @@ class Solution:
     def partitionString(self, s: str) -> int:
         chars = set()
         j = 0
-        i = 0
 
         for i in range(len(s)):
             if s[i] in chars:
                 chars.clear()
                 j += 1
             chars.add(s[i])
-            i += 1
         return j + 1
